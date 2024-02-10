107eb5ca67894088af86ceca4f60c77e8428a6b0
BionicBees <phoebescalise1@gmail.com>
2024-02-10 13:37:07 -0800

    Functioning autonomous, refactor, etc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/standardTwoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/standardTwoDriver.java
@@ -2,9 +2,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
 import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -20,8 +18,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Config
-@TeleOp(name = "Field Oriented (Two-Driver)", group = "Competition")
-public class fieldOriented extends LinearOpMode {
+@TeleOp(name = "Standard (Two-Driver)", group = "Competition")
+public class standardTwoDriver extends LinearOpMode {
 
     public static Orientation angles;
     public static Acceleration gravity;
